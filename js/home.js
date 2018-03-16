4673436d4b70e52c69d3d2cc94d1674d9f643e69
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2018-03-16 17:42:57 +0100

    Additional fixes and responsive amends

--- js/home.js
+++ js/home.js
@@ -2,6 +2,18 @@ $(window).on('resize', function() {
 	heroHeightCalculation();
 }).trigger('resize');
 
+$('.js-scroll-trigger').on('click', function(e) {
+	var $this = $(this),
+		parentSection = $this.closest('.js-scroll-section'),
+		nextSection = parentSection.next('.js-scroll-section');
+
+	$('html, body').animate({
+		scrollTop: nextSection.offset().top
+	}, 500);
+
+	e.preventDefault();
+});
+
 function heroHeightCalculation() {
 	var winHeight = $(window).height(),
 		hero = $('.hero');
